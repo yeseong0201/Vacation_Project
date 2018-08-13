f2330c5885460f67ceff829bcbb7ee658e957b0c
yeseong0201 <ys020121@naver.com>
2018-08-14 03:37:46 +0900

    first commit

---
+++ app/src/main/java/com/example/ys020/vacation_project/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.ys020.vacation_project;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    private MyView vw;
+
+    public class Vertex {
+        Vertex(float ax, float ay, Boolean ad) {
+            x = ax;
+            y = ay;
+            Draw = ad;
+        }
+
+        float x;
+        float y;
+        boolean Draw;
+    }
+
+    ArrayList<Vertex> arVertex;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        vw = new MyView(this);
+        setContentView(vw);
+        arVertex = new ArrayList<Vertex>();
+    }
+
+    protected class MyView extends View {
+        Paint pnt;
+
+        public MyView(Context context) {
+            super(context);
+            pnt = new Paint();
+            pnt.setColor(Color.BLUE);
+            pnt.setStrokeWidth(3);
+            pnt.setAntiAlias(true);
+        }
+
+        public void onDraw(Canvas canvas) {
+            int i;
+            canvas.drawColor(Color.LTGRAY);
+            for (i = 0; i < arVertex.size(); i++) {
+                if (arVertex.get(i).Draw) {
+                    canvas.drawLine(arVertex.get(i - 1).x, arVertex.get(i - 1).y, arVertex.get(i).x, arVertex.get(i).y, pnt);
+                }
+            }
+        }
+
+        public boolean onTouchEvent(MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                arVertex.add(new Vertex(event.getX(), event.getY(), false));
+                return true;
+            }
+            if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                arVertex.add(new Vertex(event.getX(), event.getY(), true));
+                invalidate();
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
